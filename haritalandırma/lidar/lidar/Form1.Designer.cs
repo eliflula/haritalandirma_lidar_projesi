8592ed30b29e332a6833aa3be61a2cbd22fa90ad
Elif Tuğba OLCAR <83315932+eliflula@users.noreply.github.com>
2022-06-28 14:05:37 +0300

    haritalandırma
    latitude longıtude kullanarak dünya haritasında istenen konuma gitme

---
+++ haritalandırma/lidar/lidar/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿using GMap.NET.WindowsForms;
+
+namespace lidar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.map = new GMap.NET.WindowsForms.GMapControl();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnGetRouteİnfo = new System.Windows.Forms.Button();
+            this.btnClearList = new System.Windows.Forms.Button();
+            this.btnAddPoint = new System.Windows.Forms.Button();
+            this.btnLoadIntoMap = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtlng = new System.Windows.Forms.TextBox();
+            this.txtlat = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+           this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(0, 0);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(497, 450);
+            this.gMapControl1.TabIndex = 0;
+            this.gMapControl1.Zoom = 0D;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(642, 612);
+            this.splitter1.TabIndex = 1;
+            this.splitter1.TabStop = false;
+            // 
+            // map
+            // 
+            this.map.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.map.Bearing = 0F;
+            this.map.CanDragMap = true;
+            this.map.EmptyTileColor = System.Drawing.Color.Navy;
+            this.map.GrayScaleMode = false;
+           // this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.map.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.map.LevelsKeepInMemory = 5;
+            this.map.Location = new System.Drawing.Point(0, 0);
+            this.map.MarkersEnabled = true;
+            this.map.MaxZoom = 18;
+            this.map.MinZoom = 2;
+            this.map.MouseWheelZoomEnabled = true;
+           // this.map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.map.Name = "map";
+            this.map.NegativeMode = false;
+            this.map.PolygonsEnabled = true;
+            this.map.RetryLoadTile = 0;
+            this.map.RoutesEnabled = true;
+           // this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.map.ShowTileGridLines = true;
+            this.map.Size = new System.Drawing.Size(785, 612);
+            this.map.TabIndex = 2;
+            this.map.Zoom = 13D;
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btnGetRouteİnfo);
+            this.panel1.Controls.Add(this.btnClearList);
+            this.panel1.Controls.Add(this.btnAddPoint);
+            this.panel1.Controls.Add(this.btnLoadIntoMap);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtlng);
+            this.panel1.Controls.Add(this.txtlat);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Location = new System.Drawing.Point(791, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(326, 612);
+            this.panel1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(34, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "majid";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnGetRouteİnfo
+            // 
+            this.btnGetRouteİnfo.BackColor = System.Drawing.SystemColors.InfoText;
+            this.btnGetRouteİnfo.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnGetRouteİnfo.Location = new System.Drawing.Point(133, 208);
+            this.btnGetRouteİnfo.Name = "btnGetRouteİnfo";
+            this.btnGetRouteİnfo.Size = new System.Drawing.Size(97, 23);
+            this.btnGetRouteİnfo.TabIndex = 8;
+            this.btnGetRouteİnfo.Text = "Get Route";
+            this.btnGetRouteİnfo.UseVisualStyleBackColor = false;
+            this.btnGetRouteİnfo.Click += new System.EventHandler(this.btnGetRouteİnfo_Click);
+            // 
+            // btnClearList
+            // 
+            this.btnClearList.BackColor = System.Drawing.SystemColors.InfoText;
+            this.btnClearList.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnClearList.Location = new System.Drawing.Point(133, 179);
+            this.btnClearList.Name = "btnClearList";
+            this.btnClearList.Size = new System.Drawing.Size(97, 23);
+            this.btnClearList.TabIndex = 7;
+            this.btnClearList.Text = "Clear";
+            this.btnClearList.UseVisualStyleBackColor = false;
+            this.btnClearList.Click += new System.EventHandler(this.btnClearList_Click);
+            // 
+            // btnAddPoint
+            // 
+            this.btnAddPoint.BackColor = System.Drawing.SystemColors.InfoText;
+            this.btnAddPoint.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnAddPoint.Location = new System.Drawing.Point(133, 149);
+            this.btnAddPoint.Name = "btnAddPoint";
+            this.btnAddPoint.Size = new System.Drawing.Size(97, 23);
+            this.btnAddPoint.TabIndex = 6;
+            this.btnAddPoint.Text = "Add Point";
+            this.btnAddPoint.UseVisualStyleBackColor = false;
+            this.btnAddPoint.Click += new System.EventHandler(this.btnAddPoint_Click);
+            // 
+            // btnLoadIntoMap
+            // 
+            this.btnLoadIntoMap.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLoadIntoMap.BackColor = System.Drawing.SystemColors.InfoText;
+            this.btnLoadIntoMap.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnLoadIntoMap.Location = new System.Drawing.Point(136, 119);
+            this.btnLoadIntoMap.Name = "btnLoadIntoMap";
+            this.btnLoadIntoMap.Size = new System.Drawing.Size(97, 23);
+            this.btnLoadIntoMap.TabIndex = 5;
+            this.btnLoadIntoMap.Text = "Load into Map";
+            this.btnLoadIntoMap.UseVisualStyleBackColor = false;
+            this.btnLoadIntoMap.Click += new System.EventHandler(this.btnLoadIntoMap_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(132, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "MAPPİNG";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(45, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Longitude";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(45, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Latitude";
+            // 
+            // txtlng
+            // 
+            this.txtlng.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtlng.BackColor = System.Drawing.SystemColors.InfoText;
+            this.txtlng.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtlng.Location = new System.Drawing.Point(133, 84);
+            this.txtlng.Name = "txtlng";
+            this.txtlng.Size = new System.Drawing.Size(97, 20);
+            this.txtlng.TabIndex = 1;
+            this.txtlng.TextChanged += new System.EventHandler(this.txtlng_TextChanged);
+            // 
+            // txtlat
+            // 
+            this.txtlat.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtlat.BackColor = System.Drawing.SystemColors.InfoText;
+            this.txtlat.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtlat.Location = new System.Drawing.Point(133, 45);
+            this.txtlat.Name = "txtlat";
+            this.txtlat.Size = new System.Drawing.Size(97, 20);
+            this.txtlat.TabIndex = 0;
+            this.txtlat.TextChanged += new System.EventHandler(this.txtlat_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 612);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.map);
+            this.Controls.Add(this.splitter1);
+           // this.Controls.Add(this.gMapControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private GMapControl gMapControl1;
+
+        // private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private System.Windows.Forms.Splitter splitter1;
+      //  private GMap.NET.WindowsForms.GMapControl map;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnLoadIntoMap;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtlng;
+        private System.Windows.Forms.TextBox txtlat;
+        private System.Windows.Forms.Button btnGetRouteİnfo;
+        private System.Windows.Forms.Button btnClearList;
+        private System.Windows.Forms.Button btnAddPoint;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ haritalandırma/lidar/lidar/Form1.cs
@@ -0,0 +1,92 @@
+﻿using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lidar
+{
+    public partial class Form1 : Form
+    {
+        private List<PointLatLng> _points;
+        public Form1()
+        {
+            
+
+            InitializeComponent();
+            _points= new List<PointLatLng>();
+        }
+
+        private void btnLoadIntoMap_Click(object sender, EventArgs e)
+        {
+            map.DragButton = MouseButtons.Left;
+            map.MapProvider = GMapProviders.GoogleMap;
+            double lat = Convert.ToDouble(txtlat.Text);
+            double lng = Convert.ToDouble(txtlng.Text);
+            map.Position = new GMap.NET.PointLatLng(lat, lng);
+            map.MinZoom = 2;
+            map.MaxZoom = 18;
+            map.Zoom = 13;
+            PointLatLng point = new PointLatLng(lat, lng);
+            GMapMarker marker = new GMarkerGoogle(point, GMarkerGoogleType.green); ;
+
+            GMapOverlay markers = new GMapOverlay("markers");
+
+            markers.Markers.Add(marker);
+            map.Overlays.Add(markers);
+
+
+
+        }
+
+        private void btnAddPoint_Click(object sender, EventArgs e)
+        {
+            _points.Add(new PointLatLng(Convert.ToDouble(txtlat.Text), Convert.ToDouble(txtlng.Text)));
+            MessageBox.Show(string.Join(",",_points));
+        }
+
+        private void btnClearList_Click(object sender, EventArgs e)
+        {
+            _points.Clear();
+        }
+
+        private void btnGetRouteİnfo_Click(object sender, EventArgs e)
+        {
+
+
+            PointLatLng route1 = _points[0];
+            PointLatLng route2 = _points[1];
+         
+
+
+            var route = GoogleMapProvider.Instance.GetRoute(route1, route2, false, false,14);
+            var r = new GMapRoute(route.Points, "my route");
+            
+            var routes = new GMapOverlay("routers");
+            routes.Routes.Add(r);
+            map.Overlays.Add(routes);
+                {
+                Pen Stroke = new Pen(Color.DarkRed, 5);
+            };
+
+        }
+
+        private void txtlat_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtlng_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
